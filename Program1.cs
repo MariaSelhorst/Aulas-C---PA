b80c28b84a537ee082d6bb6f3478fe5c4029b1d3
Maria <142902000+MariaSelhorst@users.noreply.github.com>
2024-05-21 09:55:37 -0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,21 @@
+﻿//1. Crie um programa que leia dois números inteiros do usuário e exiba a
+//soma, subtração, multiplicação e divisão desses números.
+
+using System;
+
+class Calculator 
+{
+    static void Main()
+    {
+        Console.WriteLine("\n ------Calculadora------");
+        Console.WriteLine("Insira o 1° numero: ");
+        int num1 = Convert.ToUInt16(Console.ReadLine());
+        Console.WriteLine("Insira o 2° numero: ");
+        int num2 = Convert.ToUInt16(Console.ReadLine());
+
+        Console.WriteLine($"\nA soma de {num1} e {num2} é: " + (num1 + num2));
+        Console.WriteLine($"\nA subtração de {num1} e {num2} é: " + (num1 - num2));
+        Console.WriteLine($"\nA multiplicação de {num1} e {num2} é: " + (num1 * num2));
+        Console.WriteLine($"\nA divisão de {num1} e {num2} é: " + (num1 / num2));
+    }
+}
